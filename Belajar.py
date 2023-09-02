a9bb90cfa083936248f0b81ec11d3df4d5642fd9
AR'RAFFI ABQORI NUR AZIZI <76115858+robet31@users.noreply.github.com>
2023-09-02 11:00:04 +0700

    Add files via upload

---
+++ Belajar.py
@@ -0,0 +1,28 @@
+#variable dan tipe data
+
+import rlcompleter
+
+
+x = 5 #integer
+y = 10 
+z = "saya" #string
+a = "45"
+#float
+b = 3.14
+d = 5
+#Boolean True or False
+
+#Aritmatika  
+
+c   = x + y
+print("hasil = penjumlahan",c)
+g = d // b
+print("hasil = pengurangan",d)
+r = x-y
+print("hasil = penjumlahan",r)
+#penjumlahan +
+#pengurangan -
+#perkalian *
+#pembagian / 
+#pembagian //
+#modulus %
